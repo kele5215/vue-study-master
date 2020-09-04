cac4544248027f955457f59447249154847e1131
kele5215 <7523916@qq.com>
2020-09-05 01:08:17 +0900

    eslint+prettier统一代码风格

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   env: {
-    browser: true
+    browser: true,
+    node: true,
+    jasmine: true,
+    jest: true,
+    es6: true,
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevent
@@ -13,20 +17,33 @@ module.exports = {
     // `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
+    'plugin:prettier/recommended',
   ],
   // required to lint *.vue files
-  plugins: ['vue'],
+  plugins: ['vue', 'prettier', 'html'],
   globals: {
-    NODE_ENV: false
+    NODE_ENV: false,
   },
   rules: {
+    'prettier/prettier': ['error'],
+    // "prettier/prettier": [
+    //   "error",
+    //   {
+    //     endOfLine: "auto",
+    //   },
+    // ],
+    // 这里可以设置规则：
+    // " off" or 0 - 关闭规则
+    // " warn" or 1 - 将规则作为警告打开（不影响退出代码）
+    // " error" or 2 - 将规则作为错误打开（退出代码为1）
+    'no-console': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // 添加，分号必须
-    semi: ['error', 'always'],
+    //semi: ['error', 'always'],
     'no-unexpected-multiline': 'off',
     'space-before-function-paren': 0,
     // 'space-before-function-paren': ['error', 'never'],
@@ -35,8 +52,8 @@ module.exports = {
       'error',
       'single',
       {
-        avoidEscape: true
-      }
-    ]
-  }
-};
+        avoidEscape: true,
+      },
+    ],
+  },
+}
